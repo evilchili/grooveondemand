5f874c74318a4d7e6a69c450bf120ca8a7b391a4
evilchili <evilchili@gmail.com>
2022-11-19 17:23:47 -0800

    adding test coverage reports

--- groove/auth.py
+++ groove/auth.py
@@ -2,6 +2,9 @@ import logging
 import os
 
 
-def is_authenticated(username, password):
+def is_authenticated(username: str, password: str) -> bool:
+    """
+    Returns True if the supplied username/password matches the environment.
+    """
     logging.debug(f"Authentication attempt for {username}, {password}")
     return (username == os.environ.get('USERNAME') and password == os.environ.get('PASSWORD'))
